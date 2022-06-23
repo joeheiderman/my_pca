a815ecf326cd0f02cfc7ee46f2c0569813c4dad0
joeheiderman <73437198+joeheiderman@users.noreply.github.com>
2022-06-23 09:21:23 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -338,12 +338,11 @@ namespace PCA9685 {
     /**
      * Used to set the duty cycle (0-100) of a given led connected to the PCA9685
      * @param chipAddress [64-125] The I2C address of your PCA9685; eg: 64
-     * @param ledNumber The number (1-16) of the LED to set the duty cycle on
+     * @param ledNumber The number (0-15) of the LED to set the duty cycle on
      * @param dutyCycle The duty cycle (0-100) to set the LED to
      */
     //% block
-    //% ledNum.min=0 ledNum.max= 15 
-    //% dutyCycle.min=0 dutyCycle.max=100
+    //% ledNum.min=0 ledNum.max= 15 dutyCycle.min=0 dutyCycle.max=100
     export function setLedDutyCycle(ledNum: number = 0, dutyCycle: number, chipAddress: number = 0x40): void {
         ledNum = Math.max(0, Math.min(15, ledNum))
         dutyCycle = Math.max(0, Math.min(100, dutyCycle))
